d452006002ca2943b9b476c56620a14d1781c458
AlessioAbbati <alex_117@live.it>
2023-05-05 15:14:50 +0200

    add photo with data

--- myscript.js
+++ myscript.js
@@ -1,10 +1,11 @@
-const app = Vue.createApp ({
-    data () {
+const app = Vue.createApp({
+    data() {
         return {
             message: 'HELLO',
+            photo: 'img/my-mother-was-a-saint-photo-u1.jpg'
         }
     }
-    
+
 });
 
 app.mount('#app');
